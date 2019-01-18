203973f2c080db74bdf78eec1e1b66b53c802be7
Eugênio Fonseca <eugeniofonseca14@gmail.com>
2019-01-18 03:05:47 -0200

    Initial version.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # redditFetcher
-RedditDiscordFetcher
+A run once script that fetches an image from some subreddit and posts to Discord.
+
+# Configuration
+The configuration for this script requires a valid bot credential for Discord (they call it token), a subreddit name, a criteria to specify which subset of posts the bot will query the subreddit for (ie new, controversial, hot, etc) and a valid Discord text chat room that the bot can write to so it can send the extracted image url.
+
+Check `config.example.json` for a template.
+
+# TO DO
+* Support other media.
+
+# To run
+Tested on Node.Js 11.6.0.
+
+* Run `npm install`;
+* Copy `config.example.json` in `config.json` and set it up properly;
+* `node index.js` runs the script.
+
+You can combine this with cronjob to create a periodic task.
+
+# Reference
+* [Discord.Js](https://discord.js.org/)
+* [Discord API Specs](https://discordapp.com/developers/docs/intro)
---
+++ index.js
@@ -0,0 +1,74 @@
+const fs = require("fs");
+const path = require("path");
+const https = require('https');
+const Discord = require('discord.js');
+
+// User config from your JSON at the root directory
+const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), "utf-8"));
+const discordClient = new Discord.Client();
+let discordReady = false;
+
+// Fetches an image from a sub reddit using their .json end point and passes it to a callback function
+function fetchImageFromReddit(subreddit, criteria, cb) {
+    const imageFormats = ["jpg", "jpeg", "png", "gif", "bmp"]; // Allowed formats
+    // Request
+    https.get(`https://www.reddit.com/r/${subreddit}/${criteria}/.json`, (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on("data", (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received.
+        resp.on("end", () => {
+            const finalJSON = JSON.parse(data);
+            let imageFound = false;
+            let imageUrl = "";
+            // Randomly picks a post and tries to extract it's image (direct URL), if fail tries again until succeeds.
+            while (!imageFound) {
+                const rand = Math.ceil(Math.random() * 10000 % finalJSON.data.children.length);
+                imageUrl = finalJSON.data.children[rand].data.url;
+                const urlExtension = imageUrl.split(".");
+                if (imageFormats.includes(urlExtension.pop())) {
+                    imageFound = true;
+                }
+            }
+            // Returns the image url
+            cb(imageUrl, null);
+        });
+
+    }).on("error", (err) => {
+        console.error(`Error: ${err.message}`);
+        cb(null, err);
+    });
+}
+
+// Discord module.
+discordClient.on('ready', () => {
+    console.log(`Logged in Discord as ${discordClient.user.tag}!`);
+    discordClient.targetRoom = discordClient.channels.find(x => x.id == config.channelId);
+    discordReady = true;
+
+    if (!discordClient.targetRoom) {
+        console.error("No Discord target room found, check the room id in configs or if the bot can actually see it.");
+        process.exit(1);
+    }
+});
+
+// Logins in to Discord
+discordClient.login(config.token);
+
+// Fetches the image from a subreddit and waits for Discord to be ready so it can be sent to the appropriate text channel.
+fetchImageFromReddit(config.subreddit, config.criteria, (url, error) => {
+    if (!error) {
+        let intervalId = setInterval(() => {
+            if (discordReady) {
+                discordClient.targetRoom.send(url);
+                clearInterval(intervalId);
+            }
+        }, 1000);
+    } else {
+        console.error("An error has happened while the image was being fetched from the requested subreddit.");
+    }
+});
